0134dadb7759c53f88ac66f3f45cd4996235345a
Kato Takashi <taka4.kato@gmail.com>
2019-03-16 07:25:57 +0900

    使用例とfs.jsの引数を追加（エラー処理）

--- readCSV/csvToKeireki.js
+++ readCSV/csvToKeireki.js
@@ -1,3 +1,4 @@
+//使用例　node csvToKeireki.js sample.csv
 // CSVモジュールを取り込む
 var CSV = require('csv-lite'),
     fs = require('fs'),
@@ -15,16 +16,18 @@ var faileName = process.argv[2];
 // meibo.csv(Shift_JIS)を読み込む
 var inputData = CSV.readFileSync(faileName, 'sjis');
 
-var MY_YEAR = 0, //CSVデータの年代　行
-    MY_EVENT = 1, //CSVデータの出来事　行
-    SetHTML = "";　//生成したHTMLタグを格納する文字列
+var MY_EVENT = 2 //ループの止める定数
+var SetHTML = "";
+
 
 for(var i=1; i<inputData.length;i++){
-      //HTMLのタグを追加 -> 文字列に格納
-      SetHTML += "\n<tr>\n\t<td class=\"year\">" + inputData[i][MY_YEAR] + "</td>";
-      SetHTML += "\n\t<td>" + inputData[i][MY_EVENT] + "</td>\n</tr>";
+      SetHTML += "\n<tr>\n\t<td class=\"year\">" + inputData[i][0] + "</td>";
+      SetHTML += "\n\t<td>" + inputData[i][1] + "</td>\n</tr>";
 }
 
 console.log(SetHTML);
 //.txtファイルに書き出し
-fs.writeFile(moment().format("YYYY-MM-DD")+'keireki.txt', SetHTML);
+fs.writeFile(moment().format("YYYY-MM-DD")+'keireki.txt', SetHTML,
+  function(err, result) {
+        if(err) console.log('error', err);
+      });
